d77be0eddded0c2649fd8529c0de4168bda11c6d
lloydcoder <140634407+LloydCoder@users.noreply.github.com>
2026-09-10 23:52:13 +0100

    feat: expose M14 productization core

---
+++ apps/web/lib/productization/index.ts
@@ -0,0 +1,2 @@
+export { M14_POLICY_VERSION, M14_SCORE_POLICY, M14_STATES, createObservation, createPattern, createOpportunity, addEvidence, score, transition, listOpportunities, recurrenceDecision, scoreOpportunity, validateTransition } from "./m14";
+export type { M14State, Principal } from "./m14";
